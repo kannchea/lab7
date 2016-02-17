fd7e8194ab114214ccbe149393b0e21454d41122
kannchea <luchea@ucsd.edu>
2016-02-17 21:20:34 +0000

    added coin flip

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ exports.view = function(req, res){
   	var randome_num = Math.random();
     console.log(randome_num);
     
-    if (randome_num > 0.5) {
+    if (randome_num > 0.5) 
+    {
         projects["grid"] = false;
         res.render('index', projects);
     } 
@@ -19,6 +20,14 @@ exports.view = function(req, res){
 };
 
 exports.viewGrid = function(req, res){
-  	projects["grid"] = true;
-    res.render('index', projects);
+    var randome_num = Math.random();
+  	if (randome_num > 0.5) 
+    {
+        projects["grid"] = true;
+        res.render('index', projects);
+    } 
+    else
+    {
+        res.redirect('/');
+    }
 };
